748b8f0bfc655e84ac01273fbdc8c34c90bd6311
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-03-21 21:49:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,61 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-
+import "./App.css";
 
 function App() {
-
   const [colour, setColour] = useState("red");
   const colorClasses = {
     red: "bg-red-500",
     blue: "bg-blue-500",
     green: "bg-green-500",
-    yellow: "bg-yellow-500"
+    yellow: "bg-yellow-500",
   };
-  
 
   return (
-    <>
-     <div className={`${colorClasses[colour]}`}> My colour is now {colour}</div>
-     <button
-     onClick={()=>{setColour("blue")}}
+    <main className={`${colorClasses[colour]}`}>
+      <div className={`${colorClasses[colour]} p-15  rounded-4xl`}>
+        {" "}
+        My colour is now {colour}
+      </div>
+      <div className="pt-2">
+        <button
+          onClick={() => {
+            setColour("blue");
+          }}
+        >
+          Click Me to make blue
+        </button>
+      </div>
+      <div className="pt-2">
+        <button
+          onClick={() => {
+            setColour("red");
+          }}
+        >
+          Click Me to make red
+        </button>
+      </div>
+      <div className="pt-2">
+        <button
+          onClick={() => {
+            setColour("green");
+          }}
+        >
+          Click Me to make green
+        </button>
+      </div>
 
-     >Click Me to make blue</button>
-     
-    </>
-  )
+      <div className="pt-2">
+        <button
+          onClick={() => {
+            setColour("yellow");
+          }}
+        >
+          click me to make yellow
+        </button>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
